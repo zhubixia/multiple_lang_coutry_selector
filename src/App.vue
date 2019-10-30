f35d4e55bcb51fa914433bd5930d1cc4843157af
zhubixia <zhubixia@xgd.com>
2019-10-30 14:17:42 +0800

    多语言国家下拉框

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <country-selector v-model="selected" width="260" language="en"></country-selector>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CountrySelector from 'vue-country-selector'
 export default {
   name: 'app',
+  data() {
+    return {
+      country: [
+          {
+              code: 'CN',             // id
+              cnName: '中国',        // 中文名
+              cnSpell: 'zhongguo',    // 中文拼音（大小写均可）
+              enName: 'China',        // 英文名
+              hot: true               // 是否热门，值为true的国家将显示在HOTtab下
+          },
+          {
+              code: 'us',
+              cnName: '美国',
+              cnSpell: 'meiguo',
+              enName: 'United States of America',
+              hot: true
+          },
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    CountrySelector
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CountrySelector from 'vue-country-selector'
+import 'vue-country-selector/dist/countryselector.css'
 
+Vue.use(CountrySelector)
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
